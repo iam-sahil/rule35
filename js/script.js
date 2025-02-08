a819a779d2cbc77bf63859445cd1b6e3d83cc431
iam-sahil <sss.lata82@gmail.com>
2025-02-09 01:09:48 +0530

    download button padding

--- js/script.js
+++ js/script.js
@@ -427,13 +427,11 @@ function displayPinnedImage(image) {
     const downloadLink = document.createElement('a');
     downloadLink.classList.add('download-btn');
     downloadLink.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-             fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/>
-            <path d="m14 19 3 3v-5.5"/>
-            <path d="m17 22 3-3"/>
-            <circle cx="9" cy="9" r="2"/>
-        </svg>`;
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/>
+        <path d="m17 22 3-3"/>
+        <circle cx="9" cy="9" r="2"/></svg>`;
     downloadLink.href = image.full;
     downloadLink.addEventListener('click', function(e) {
         e.preventDefault();
@@ -461,14 +459,12 @@ async function displayImages(images) {
   
           const downloadLink = document.createElement('a');
           downloadLink.classList.add('download-btn');
-          downloadLink.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                 fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/>
-              <path d="m14 19 3 3v-5.5"/>
-              <path d="m17 22 3-3"/>
-              <circle cx="9" cy="9" r="2"/>
-            </svg>`;
+      downloadLink.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+        fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/>
+        <path d="m17 22 3-3"/>
+        <circle cx="9" cy="9" r="2"/></svg>`;
           downloadLink.href = image.full; // Keep full image for download
           downloadLink.addEventListener('click', function(e) {
               e.preventDefault();
